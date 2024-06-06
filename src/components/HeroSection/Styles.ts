e1b872c5ec1a30cc24b59352c1fa18dea99029cc
pawelek <k.pawelek@vp.pl>
2024-06-06 17:55:35 +0200

    Update Styles.ts

--- src/components/HeroSection/Styles.ts
+++ src/components/HeroSection/Styles.ts
@@ -23,14 +23,12 @@ export const HeadingWrapper = styled.div`
 display: flex;
 flex-direction: column;
 align-items: flex-end;
- position: absolute;
- bottom: 20%;
- right: 5%;
+height: 50%;
+justify-content: flex-end;
 `
 
 export const Section = styled.section`
-height: 100vh;
-padding-top: 9rem;
+padding-top: 8rem;
 position: relative;
   -webkit-user-select: none; /* Safari /
   -ms-user-select: none; / IE 10 and IE 11 /
@@ -38,9 +36,9 @@ position: relative;
 `
 
 export const ImageContainer = styled.div`
-  position: relative;
+  padding: 0rem 1rem;
   width: 100%;
-  height: 800px;
+  height: 100vh;
   background: radial-gradient(circle at center, rgba(0, 0, 0, 73%) 20%, var(--background) 40%),
     url(${backgroundImage});
   background-size: cover;
