2c0c1a6469e8a72bb729c16421b926966d617d09
pawelek <k.pawelek@vp.pl>
2024-06-07 16:41:45 +0200

    add parallax styles

--- src/components/Parallax/Styles.ts
+++ src/components/Parallax/Styles.ts
@@ -15,15 +15,21 @@ overflow: hidden;
   text-transform: uppercase;
   font-size: 64px;
   display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: .8rem;
   white-space: nowrap;
   display: flex;
   flex-wrap: nowrap;
+ background: rgb(0,0,0);
+background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(76,75,2,1) 39%, rgba(74,73,13,1) 50%, rgba(69,67,1,1) 65%, rgba(0,0,0,1) 100%);
 }
 
  span {
   display: block;
   margin-right: 30px;
-  color: var(--secondary);
+  color: black;
+  letter-spacing: 1px;
 }
 
 `
