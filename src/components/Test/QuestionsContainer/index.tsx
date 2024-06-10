43095b318b8fdc5ca4fd29a11ccfd66e295fa740
pawelek <k.pawelek@vp.pl>
2024-06-10 11:48:44 +0200

    add fetch to api

--- src/components/Test/QuestionsContainer/index.tsx
+++ src/components/Test/QuestionsContainer/index.tsx
@@ -1,6 +1,38 @@
+import { useEffect, useReducer } from "react";
 import { Heading } from "../../Heading/h2";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unkonwn");
+  }
+};
+
 export const TestContainer = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((e) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <section>
       <Heading content={"Are you ready?"} visually={true} />
