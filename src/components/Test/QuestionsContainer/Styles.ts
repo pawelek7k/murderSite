fabc8eb329dbb4ce962b53634373013691acb0d8
pawelek <k.pawelek@vp.pl>
2024-06-12 14:46:00 +0200

    add styles test

--- src/components/Test/QuestionsContainer/Styles.ts
+++ src/components/Test/QuestionsContainer/Styles.ts
@@ -1,3 +1,14 @@
 import styled from "styled-components";
+import backgroundImage from '../../../images/test_bg.jpg';
 
-export const QuestionsContStyles = styled.section``
+export const QuestionsContStyles = styled.section`
+    height: 100vh;
+    padding: 8rem 3rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    background: linear-gradient(to bottom, var(--background) 0%, var(--background) 20%, rgba(0, 0, 0, 0.84) 100%), url(${backgroundImage});
+  background-size: cover;
+  background-position: center;
+`
