c31b132cce48c7c30c620bccd82494aa5f13f62b
pawelek <k.pawelek@vp.pl>
2024-06-21 10:53:09 +0200

    add date container

---
+++ src/components/Date/Styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    position: absolute;
+    top: 50%;
+    bottom: 50px;
+`
---
+++ src/components/Date/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from "./Styles";
+
+export const Date = () => {
+  return <Container>data</Container>;
+};
