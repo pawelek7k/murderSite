4585c85a4edfc708734b274783ec175dcb68d935
pawelek <k.pawelek@vp.pl>
2024-07-02 14:05:45 +0200

    Update Result.tsx

--- src/components/Test/QuestionsContainer/Questions/Result.tsx
+++ src/components/Test/QuestionsContainer/Questions/Result.tsx
@@ -1,6 +1,42 @@
+import { Heading } from "../../../Heading/h2";
+
 interface ResultProps {
   points: number;
 }
+
 export const Result = ({ points }: ResultProps) => {
-  return <div>{points}</div>;
+  let category = "";
+  let description = "";
+
+  switch (true) {
+    case points < 50:
+      category = "Person with healthy personality";
+      description =
+        "Your results indicate a healthy personality with no significant psychopathic tendencies.";
+      break;
+    case points >= 50 && points <= 100:
+      category = "Person with moderate psychopathic tendencies";
+      description =
+        "Your results indicate some traits that may be associated with psychopathy. Further observation and possible consultation with a psychologist are recommended.";
+      break;
+    case points > 100 && points <= 200:
+      category = "Person with high psychopathic tendencies";
+      description =
+        "Your results indicate a high level of traits associated with psychopathy. It is recommended to consult with a psychologist or psychiatrist for a more thorough evaluation.";
+      break;
+    default:
+      category = "Error";
+      description =
+        "The score exceeds the expected range. Please check the answers.";
+  }
+
+  return (
+    <div>
+      <Heading content={"Test Complete!"} visually={true} />
+      <ul>
+        <li>{category}</li>
+        <li>{description}</li>
+      </ul>
+    </div>
+  );
 };
