33ec26bfef00d11d50695009241ebaca3446a542
pawelek <k.pawelek@vp.pl>
2024-07-09 23:13:13 +0200

    Update index.tsx

--- src/components/Footer/Form/index.tsx
+++ src/components/Footer/Form/index.tsx
@@ -77,7 +77,7 @@ export const Form = () => {
           $isFocused={isTextAreaFocused}
           $hasValue={hasTextAreaValue}
         >
-          How can we help you?
+          How can we help you??
         </Label>
       </InputField>
       <Button content={"Submit"} bg={true} />
